6007a13af60aa474028af22e3e96f7e24075a3cd
devclyde <israel.devclyde@protonmail.com>
2022-06-18 04:08:36 -0300

    feat: emit basic signals

--- awchad/init.lua
+++ awchad/init.lua
@@ -1,5 +1,14 @@
 require('awchad.wm-events')
 
+awesome.emit_signal('wm::pre_setup')
+
+require('awchad.signals.naughty')
+require('awchad.signals.ruled.client')
+require('awchad.signals.ruled.notification')
+require('awchad.signals.tag')
+
+awesome.emit_signal('wm::post_setup')
+
 ---@type table
 awesome = awesome
 ---@type table
