2dd8bd2a89bd11d701fbe7b6488e0775e123374a
devclyde <israel.devclyde@protonmail.com>
2022-06-18 04:15:35 -0300

    feat: load theme

--- awchad/wm-events.lua
+++ awchad/wm-events.lua
@@ -1,8 +1,11 @@
+local beautiful = require('beautiful')
 local gears = require('gears')
 
 awesome.connect_signal('wm::pre_setup', function()
   require('awful.autofocus')
   require('awful.hotkeys_popup.keys')
+
+  beautiful.init(require('awchad.theme'))
 end)
 
 awesome.connect_signal('wm::post_setup', function()
