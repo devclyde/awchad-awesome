08875d24c07c39f1c45456cc7800c98c1d3ea373
devclyde <israel.devclyde@protonmail.com>
2022-06-18 07:42:42 -0300

    fix: move `AWCHAD_MODIFIER`

--- awchad/init.lua
+++ awchad/init.lua
@@ -6,6 +6,17 @@ require('awchad.signals.naughty')
 require('awchad.signals.ruled.client')
 require('awchad.signals.ruled.notification')
 require('awchad.signals.tag')
+require('awchad.signals.screen.desktop')
+
+require('awchad.mappings.global.mouse')
+
+require('awchad.mappings.global.keyboard.general')
+require('awchad.mappings.global.keyboard.monitor')
+require('awchad.mappings.global.keyboard.multimedia')
+require('awchad.mappings.global.keyboard.focus')
+require('awchad.mappings.global.keyboard.layout')
+require('awchad.mappings.global.keyboard.workspaces')
+require('awchad.mappings.global.keyboard.tags')
 
 awesome.emit_signal('wm::post_setup')
 
